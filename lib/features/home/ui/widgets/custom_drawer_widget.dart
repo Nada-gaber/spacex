df077d32f09f160409bf623d7175c123f15b84cd
MAHMOUD <mahmoud.alaa1612@gmail.com>
2024-04-06 05:55:39 +0200

    finish ui for drawer

---
+++ lib/features/home/ui/widgets/custom_drawer_widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:spacex/features/home/ui/widgets/profile_section.dart';
+
+import 'drawer_component.dart';
+
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Colors.black.withOpacity(.95),
+      child: SafeArea(
+        child: Column(
+          children: [
+            const ProfileSection(),
+            const DrawerComponent(
+              title: "My Profile",
+              icon: Icons.person_pin,
+            ),
+            buildDivide(),
+            const DrawerComponent(
+              title: "Crew",
+              icon: Icons.groups,
+            ),
+            buildDivide(),
+            const DrawerComponent(
+              title: "Dragons",
+              icon: Icons.rocket_launch_outlined,
+            ),
+            buildDivide(),
+            const DrawerComponent(
+              title: "Ships",
+              icon: FontAwesomeIcons.ship,
+            ),
+            buildDivide(),
+            const DrawerComponent(
+              title: "Company Information",
+              icon: Icons.info_outline_rounded,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/home/ui/widgets/drawer_component.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/constant/colors.dart';
+
+class DrawerComponent extends StatelessWidget {
+  const DrawerComponent(
+      {super.key, required this.title, required this.icon, this.onTap});
+
+  final String title;
+  final IconData icon;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding:
+            const EdgeInsetsDirectional.symmetric(vertical: 12, horizontal: 12),
+        child: Row(
+          children: [
+            Icon(
+              icon,
+              size: screenWidth / 10,
+              color: AppColors.textColor,
+            ),
+            const SizedBox(
+              width: 12,
+            ),
+            Text(
+              title,
+              style: TextStyle(
+                  fontWeight: FontWeight.w500, fontSize: screenWidth / 18),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget buildDivide() {
+  return Padding(
+    padding: const EdgeInsetsDirectional.symmetric(horizontal: 16),
+    child: Divider(
+      height: .5,
+      color: Colors.grey.withOpacity(.2),
+    ),
+  );
+}
