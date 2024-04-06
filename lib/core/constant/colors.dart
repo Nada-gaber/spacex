66510af303932e544390fe8f6f9fa88855c43f11
MAHMOUD <mahmoud.alaa1612@gmail.com>
2024-04-06 05:13:13 +0200

    add our colors class

---
+++ lib/core/constant/colors.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+
+class AppColors{
+
+  static const backgroundColor =Color(0xff061428);
+  static const buttonColor =Color(0xff415A77);
+  static const customBlueColor =Color(0xff778DA9);
+  static const textColor =Color(0xffFFFFFF);
+  static const errorRedColor =Color(0xffFF6E6E);
+
+}
