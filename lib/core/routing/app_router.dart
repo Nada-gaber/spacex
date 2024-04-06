562f9877197228b0c74f7f2a2ed1ff8794ab1b3e
MAHMOUD <mahmoud.alaa1612@gmail.com>
2024-04-06 05:44:34 +0200

    add home screen to app router class

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:spacex/core/routing/routes.dart';
+import 'package:spacex/features/home/ui/views/home_screen.dart';
 import 'package:spacex/features/onboarding/onboarding_view.dart';
 
 class AppRouter {
-  Route? generateRoute(RouteSettings settings) {
+  Route generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case Routes.onboarding:
         return MaterialPageRoute(
           builder: (context) => const OnboardingView(),
         );
-    }
-    return null;
+
+
+      case Routes.home:
+        return MaterialPageRoute(
+          builder: (context) => const HomeScreen(),
+        );
+      default:
+        return MaterialPageRoute(
+          builder: (context) => const Scaffold(
+            body: Center(
+              child: Text("No route"),
+            ),
+          ),
+        );
+      }
+
   }
 }
--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,3 +1,5 @@
 class Routes {
   static const String onboarding = '/onboarding';
+  static const String home = '/home';
+
 }
