b4cd24c84c5e596abc89ac2fed0ed9d6b66ebfc6
Nada-gaber <chocoshaka20@gmail.com>
2024-04-07 05:28:50 +0200

    Company info changes

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:spacex/core/routing/routes.dart';
+import 'package:spacex/features/home/ui/views/company_info/company_info.dart';
 import 'package:spacex/features/onboarding/ui/onboarding_screen.dart';
 
 class AppRouter {
@@ -8,6 +9,10 @@ class AppRouter {
       case Routes.onboarding:
         return MaterialPageRoute(
           builder: (context) => const OnboardingScreen(),
+        );
+         case Routes.companyInfo:
+        return MaterialPageRoute(
+          builder: (context) => const CompanyInfo(),
         );
     }
     return null;
