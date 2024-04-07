5a0435fdeeacd794ae1cfa4b3b578939e8c45ae4
Nada-gaber <chocoshaka20@gmail.com>
2024-04-07 23:50:21 +0200

    splash staged changes

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../../core/constant/color/app_color.dart';
 import 'splash_utils/splash_navigate.dart';
-import 'splash_widgets/splash_built.dart';
+import 'splash_widgets.dart/splash_build.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
