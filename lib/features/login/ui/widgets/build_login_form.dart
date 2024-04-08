f387b765ad5f1bf3c973740191a7cdbe6d2d54d7
Mahmoud <atta89383@gmail.com>
2024-04-08 23:52:40 +0200

    handle validator

--- lib/features/login/ui/widgets/build_login_form.dart
+++ lib/features/login/ui/widgets/build_login_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spacex/core/utils/app_regex.dart';
 import 'package:spacex/core/widgets/custom_text_form_field.dart';
 
 class BuildLoginForm extends StatelessWidget {
@@ -6,19 +7,34 @@ class BuildLoginForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomFormField(
-          text: 'Email Address',
-          icon: Icons.mail_outline_outlined,
-        ),
-        SizedBox(height: 25),
-        CustomFormField(
-          text: 'Password',
-          obscureText: true,
-          icon: Icons.lock_outline_rounded,
-        ),
-      ],
+    return Form(
+      key: GlobalKey(),
+      child: Column(
+        children: [
+          CustomFormField(
+            validator: (value) {
+              if (value == null || !AppRegex.isEmailValid(value)) {
+                return 'Please enter a valid email';
+              }
+              return null;
+            },
+            text: 'Email Address',
+            icon: Icons.mail_outline_outlined,
+          ),
+          const SizedBox(height: 25),
+          CustomFormField(
+            validator: (value) {
+              if (value == null || !AppRegex.hasMinLength(value)) {
+                return 'The password is too short, please enter at least 8 character';
+              }
+              return null;
+            },
+            text: 'Password',
+            obscureText: true,
+            icon: Icons.lock_outline_rounded,
+          ),
+        ],
+      ),
     );
   }
 }
