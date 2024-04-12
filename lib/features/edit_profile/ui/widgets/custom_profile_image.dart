072a1ce6d99a954ca33c9756d0ffd01d1ee82043
MAHMOUD <mahmoud.alaa1612@gmail.com>
2024-04-12 21:20:05 +0200

    add custom app bar widget and custom profile image to profile screen

---
+++ lib/features/edit_profile/ui/widgets/custom_profile_image.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/constant/colors.dart';
+import '../../../../core/widgets/custom_icon_button.dart';
+
+class CustomProfileImage extends StatelessWidget {
+  const CustomProfileImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Stack(
+      children: [
+        CircleAvatar(
+          radius: screenWidth / 4.4,
+          backgroundColor: AppColors.customBlueColor,
+          child: CircleAvatar(
+            radius: screenWidth / 4.6,
+            backgroundImage: const NetworkImage(
+                "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"),
+          ),
+        ),
+        Positioned(
+            bottom: 5,
+            right: 0,
+            child: CustomIconButton(
+              onTap: () {},
+              size: screenWidth / 10,
+              icon: Icons.edit,
+            ))
+      ],
+    );
+  }
+}
