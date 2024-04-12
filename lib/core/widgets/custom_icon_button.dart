2bf6350dc2c475c0fee2fcec11b44bb6ca0b20a0
MAHMOUD <mahmoud.alaa1612@gmail.com>
2024-04-12 21:18:35 +0200

    edit custom icon button widget

--- lib/core/widgets/custom_icon_button.dart
+++ lib/core/widgets/custom_icon_button.dart
@@ -1,26 +1,25 @@
-
 import 'package:flutter/material.dart';
 
-import '../constant/colors.dart';
-
 class CustomIconButton extends StatelessWidget {
-  const CustomIconButton({super.key, this.onTap, required this.icon});
+  const CustomIconButton(
+      {super.key, this.onTap, required this.icon, required this.size});
 
   final void Function()? onTap;
   final IconData icon;
+  final double size;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-
       onTap: onTap,
       child: Container(
+          padding: const EdgeInsetsDirectional.all(4),
           decoration: BoxDecoration(
-              shape: BoxShape.circle, color: Colors.grey.withOpacity(.7)),
+              shape: BoxShape.circle, color: Colors.white.withOpacity(.8)),
           child: Icon(
             icon,
-            size: MediaQuery.of(context).size.width / 10,
-            color: AppColors.textColor,
+            size: size,
+            color: Colors.black,
           )),
     );
   }
