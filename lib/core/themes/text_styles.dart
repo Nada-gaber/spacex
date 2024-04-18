a26521d2e4f683deb869a883e82a4085ade45da3
MAHMOUD <mahmoud.alaa1612@gmail.com>
2024-04-18 15:42:48 +0200

    add new style to our text styles class

--- lib/core/themes/text_styles.dart
+++ lib/core/themes/text_styles.dart
@@ -12,4 +12,14 @@ class MyTextStyles {
     fontSize: 18,
     fontWeight: FontWeight.w600,
   );
+  static const TextStyle font20WhiteW600 = TextStyle(
+    color: Colors.white,
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle font18WhiteW400 = TextStyle(
+    color: Colors.white60,
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+  );
 }
