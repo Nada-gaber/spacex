1c9344e450eed4e44f63d723845e521554dc699e
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2024-04-21 23:45:16 +0200

    crew bug fixed

--- lib/features/crew/ui/views/crew_member_details_screen.dart
+++ lib/features/crew/ui/views/crew_member_details_screen.dart
@@ -13,96 +13,90 @@ class CrewMemberDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery
-        .of(context)
-        .size
-        .width;
+    double screenWidth = MediaQuery.of(context).size.width;
 
     return SafeArea(
       child: Scaffold(
         backgroundColor: AppColors.backgroundDarkBlue,
         body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Hero(
-                tag: model.imageUrl,
-                child: ClipRRect(
-                  borderRadius: BorderRadiusDirectional.circular(24),
-                  child: SizedBox(
-                    height: MediaQuery
-                        .of(context)
-                        .size
-                        .height * .85,
-                    child: Stack(fit: StackFit.expand, children: [
-                      Image.network(
-                        model.imageUrl,
-                        fit: BoxFit.cover,
-                      ),
-                      Positioned(
-                        top: 10,
-                        left: 10,
-                        child: CustomIconButton(
-                          size: screenWidth / 9.5,
-                          icon: Icons.arrow_back,
-                          onTap: () {
-                            context.pop();
-                          },
-                          isFontAwesomeIcons: false,
-                          color: Colors.black,
-                          backgroundColor: Colors.transparent,
+          padding: const EdgeInsets.only(right:16.0, left: 16.0,bottom: 16),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 16,),
+                Hero(
+                  tag: model.imageUrl,
+                  child: ClipRRect(
+                    borderRadius: BorderRadiusDirectional.circular(24),
+                    child: SizedBox(
+                      height: MediaQuery.of(context).size.height * .85,
+                      child: Stack(fit: StackFit.expand, children: [
+                        Image.network(
+                          model.imageUrl,
+                          fit: BoxFit.cover,
                         ),
-                      ),
-                      Positioned(
-                        top: 10,
-                        right: 10,
-                        child: CustomIconButton(
-                          isFontAwesomeIcons: false,
-                          size: screenWidth / 9.5,
-                          icon: Icons.info_outline,
-                          color: Colors.black,
-                          backgroundColor: Colors.transparent,
+                        Positioned(
+                          top: 10,
+                          left: 10,
+                          child: CustomIconButton(
+                            size: screenWidth / 9.5,
+                            icon: Icons.arrow_back,
+                            onTap: () {
+                              context.pop();
+                            },
+                            isFontAwesomeIcons: false,
+                            color: Colors.black,
+                            backgroundColor: Colors.transparent,
+                          ),
                         ),
-                      ),
-                    ]),
+                        Positioned(
+                          top: 10,
+                          right: 10,
+                          child: CustomIconButton(
+                            isFontAwesomeIcons: false,
+                            size: screenWidth / 9.5,
+                            icon: Icons.info_outline,
+                            color: Colors.black,
+                            backgroundColor: Colors.transparent,
+                          ),
+                        ),
+                      ]),
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      model.name,
-                      style: TextStyle(
-                          fontSize: MediaQuery
-                              .of(context)
-                              .size
-                              .width / 16,
-                          color: AppColor.textColor,
-                          fontWeight: FontWeight.w600),
-                    ),
-                    Row(
-                      children: [
-                        const Icon(Icons.work_outline_rounded),
-                        const SizedBox(
-                          width: 6,
-                        ),
-                        Text(
-                          model.agencyName,
-                          style: TextStyle(
-                              fontSize: 20,
-                              color: AppColor.textColor.withOpacity(.7),
-                              fontWeight: FontWeight.w400),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              )
-            ],
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        model.name,
+                        style: TextStyle(
+                            fontSize: MediaQuery.of(context).size.width / 16,
+                            color: AppColor.textColor,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      Row(
+                        children: [
+                          const Icon(Icons.work_outline_rounded),
+                          const SizedBox(
+                            width: 6,
+                          ),
+                          Text(
+                            model.agencyName,
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: AppColor.textColor.withOpacity(.7),
+                                fontWeight: FontWeight.w400),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
