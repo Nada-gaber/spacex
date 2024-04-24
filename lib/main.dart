93d1122cd9b5ffac1d44099bb42e77c56597a1c7
MAHMOUD <mahmoud.alaa1612@gmail.com>
2024-04-24 02:01:01 +0200

    add dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:spacex/core/routing/app_router.dart';
 import 'package:spacex/spacex_app.dart';
 
+import 'core/di/dependency_injection.dart';
+
 void main() {
+  setupGetIt();
   runApp(SpacexApp(
     appRouter: AppRouter(),
   ));
