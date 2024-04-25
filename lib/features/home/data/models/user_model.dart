fc39df8c5c7120e589510add72023786bd54b8d5
Mahmoud <atta89383@gmail.com>
2024-04-25 19:10:43 +0200

    add user model

---
+++ lib/features/home/data/models/user_model.dart
@@ -0,0 +1,31 @@
+class UserModel {
+  final String name;
+  final String email;
+  final String image;
+  final String token;
+
+  const UserModel({
+    required this.name,
+    required this.email,
+    required this.image,
+    required this.token,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      name: json['name'],
+      email: json['email'],
+      image: json['image'],
+      token: json['token'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'email': email,
+      'image': image,
+      'token': token,
+    };
+  }
+}
