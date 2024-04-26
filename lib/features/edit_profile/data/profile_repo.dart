00371aa81e223be24f4c74702dc40bd0efe5bec8
Mahmoud <atta89383@gmail.com>
2024-04-26 11:52:41 +0300

    add profile repo

---
+++ lib/features/edit_profile/data/profile_repo.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dartz/dartz.dart';
+import 'package:spacex/core/errors/cloud_firestore_failure.dart';
+import 'package:spacex/core/errors/failure.dart';
+import 'package:spacex/features/home/data/models/user_model.dart';
+
+class ProfileRepo {
+  final FirebaseFirestore firebaseFirestoreInstance;
+
+  ProfileRepo(this.firebaseFirestoreInstance);
+
+  Future<Either<Failure, Unit>> editProfileData(UserModel userModel) async {
+    try {
+      await firebaseFirestoreInstance
+          .collection('users')
+          .doc(userModel.token)
+          .set(userModel.toMap());
+      return const Right(unit);
+    } catch (error) {
+      if (error is FirebaseException) {
+        return Left(CloudFirestoreFailure.fromFirebaseCoreException(error));
+      } else {
+        return Left(CloudFirestoreFailure(error.toString()));
+      }
+    }
+  }
+}
