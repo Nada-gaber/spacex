863313a15660584fc746c9cae614b00766e58ade
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2024-04-27 19:02:03 +0200

    models

--- lib/features/company_info/data/model/company_info_model.dart
+++ lib/features/company_info/data/model/company_info_model.dart
@@ -1,1 +1,32 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'links_model.dart';
+
+part 'company_info_model.g.dart';
+
+@JsonSerializable()
+class CompanyInfo {
+  final String? name;
+  final String? ceo;
+  final int? founded;
+  final int? employees;
+  final int? vehicles;
+  final String? summary;
+  final Links? links;
+
+  CompanyInfo({
+    required this.name,
+    required this.ceo,
+    required this.founded,
+    required this.employees,
+    required this.vehicles,
+    required this.summary,
+    required this.links,
+  });
+
+  factory CompanyInfo.fromJson(Map<String, dynamic> json) =>
+      _$CompanyInfoFromJson(json);
+        Map<String, dynamic> toJson() =>  _$CompanyInfoToJson(this);
+}
+
+
 
---
+++ lib/features/company_info/data/model/company_info_model.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'company_info_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CompanyInfo _$CompanyInfoFromJson(Map<String, dynamic> json) => CompanyInfo(
+      name: json['name'] as String?,
+      ceo: json['ceo'] as String?,
+      founded: json['founded'] as int?,
+      employees: json['employees'] as int?,
+      vehicles: json['vehicles'] as int?,
+      summary: json['summary'] as String?,
+      links: json['links'] == null
+          ? null
+          : Links.fromJson(json['links'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$CompanyInfoToJson(CompanyInfo instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'ceo': instance.ceo,
+      'founded': instance.founded,
+      'employees': instance.employees,
+      'vehicles': instance.vehicles,
+      'summary': instance.summary,
+      'links': instance.links,
+    };
