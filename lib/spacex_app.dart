5f3dce0e28f29589311e5ebcb6b6ed4bec3f9842
MAHMOUD <mahmoud.alaa1612@gmail.com>
2024-04-29 22:24:15 +0300

    remove unused import

--- lib/spacex_app.dart
+++ lib/spacex_app.dart
@@ -1,10 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:spacex/core/routing/app_router.dart';
 import 'package:spacex/core/routing/routes.dart';
 import 'package:spacex/core/utils/dependency_injection.dart';
-import 'package:spacex/features/home/data/repo/home_repo.dart';
 import 'package:spacex/features/home/logic/get_profile_data_cubit.dart';
 
 class SpacexApp extends StatelessWidget {
