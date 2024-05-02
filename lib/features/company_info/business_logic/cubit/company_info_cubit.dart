95f41f45602faf6548d4d7327174ea9d29fd849e
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2024-05-02 08:11:03 +0200

    company cubit edit fetch method

--- lib/features/company_info/business_logic/cubit/company_info_cubit.dart
+++ lib/features/company_info/business_logic/cubit/company_info_cubit.dart
@@ -1,19 +1,17 @@
 import 'package:bloc/bloc.dart';
+import 'package:spacex/features/ships/business_logic/cubit/ships_states.dart';
 import '../../data/repo/company_info_repo.dart';
 import 'company_info_states.dart';
 
 class CompanyCubit extends Cubit<CompanyInfoState> {
   final CompanyRepository _companyRepository;
 
-  CompanyCubit(this._companyRepository): super(CompanyInitial());
+  CompanyCubit(this._companyRepository) : super(CompanyInitial());
 
-  Future<void> fetchCompanyInfo() async {
-    emit( CompanyLoading());
-    try {
-      final companyInfo = await _companyRepository.getCompanyInfo();
-      emit(CompanyLoaded(companyInfo ));
-    } on Exception catch (error) {
-      emit(CompanyError(error.toString()));
-    }
+  Future fetchCompanyInfo() async {
+    emit(CompanyLoading());
+    final companyInfo = await _companyRepository.getCompanyInfo();
+    companyInfo.fold((failure) => emit(CompanyError(failure.message)),
+        (companyInfo) => emit(CompanyLoaded(companyInfo)));
   }
 }
