830ace5dc2e36af797e40f32d5534ca1ea22d531
Mahmoud <atta89383@gmail.com>
2024-05-02 09:40:16 +0300

    add hive packages

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,12 @@ import 'package:spacex/core/utils/dependency_injection.dart';
 import 'package:spacex/core/utils/shared_preferences.dart';
 import 'package:spacex/firebase_options.dart';
 import 'package:spacex/spacex_app.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setupGetIt();
+  await Hive.initFlutter();
   await MySharedPreferences.init();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
