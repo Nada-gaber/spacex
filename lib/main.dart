cca9678fd6545e596e4bdaa45733ea3fc38e7687
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2024-05-10 10:59:29 +0200

    add system navigator bar and status bar color

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:spacex/core/constant/colors.dart';
 import 'package:spacex/core/routing/app_router.dart';
 import 'package:spacex/core/utils/dependency_injection.dart';
 import 'package:spacex/core/utils/shared_preferences.dart';
@@ -7,6 +9,13 @@ import 'package:spacex/firebase_options.dart';
 import 'package:spacex/spacex_app.dart';
 
 void main() async {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    systemNavigationBarColor: AppColors.backgroundDarkBlue, 
+    systemNavigationBarIconBrightness: Brightness.dark,
+    statusBarBrightness: Brightness.dark,
+    statusBarIconBrightness: Brightness.dark,
+    statusBarColor: AppColors.backgroundDarkBlue, 
+  ));
   WidgetsFlutterBinding.ensureInitialized();
   setupGetIt();
   await MySharedPreferences.init();
