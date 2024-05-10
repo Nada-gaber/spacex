b0ff0a01c0f145ba27e47d0ec45d08cd9dd7c7b1
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2024-05-10 04:38:43 +0200

    changed some constants

--- lib/core/constant/colors.dart
+++ lib/core/constant/colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const backgroundDarkBlue = Color(0xff061428);
-  static const buttonBlue = Color(0xff415A77);
+  static const backgroundDarkBlue = Color(0xFF090E14);
+  static const buttonBlue = Color(0xFF3B69FF);
   static const blueGray = Color(0xff778DA9);
   static const textWhite = Color(0xffFFFFFF);
   static const errorRed = Color(0xffFF6E6E);
--- lib/core/constant/images.dart
+++ lib/core/constant/images.dart
@@ -4,11 +4,12 @@ class MyImages {
   static const firstOnboarding = 'assets/images/first_onboarding.png';
   static const secondOnboarding = 'assets/images/second_onboarding.png';
   static const thirdOnboarding = 'assets/images/third_onboarding.png';
-  static const authBackground = 'assets/images/auth_background.png'; 
-  static const elonMask = 'assets/images/elon_mask.png';
+  static const authBackground = 'assets/images/auth_background.png';
+  static const elonMask = 'assets/images/elon.png';
   static const imageNotFound =
       'https://salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png';
-  static const profileImage = 'https://firebasestorage.googleapis.com/v0/b/spacex-e632a.appspot.com/o/profile%2Fsplash.jpg?alt=media&token=9ca83bc5-afed-4b48-b8c5-faf8e2d7bfd6';
-  static const animatedSpace= "assets/images/animated_space_image.gif";
+  static const profileImage =
+      'https://firebasestorage.googleapis.com/v0/b/spacex-e632a.appspot.com/o/profile%2Fsplash.jpg?alt=media&token=9ca83bc5-afed-4b48-b8c5-faf8e2d7bfd6';
+  static const animatedSpace = "assets/images/animated_space_image.gif";
+  static const loading = 'assets/images/loading.gif';
 }
-
