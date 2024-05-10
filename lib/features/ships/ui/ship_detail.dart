c20960e2c4ef5a6cc1579f9b88f26ef3859e0029
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2024-05-10 10:56:47 +0200

    edit ships details

--- lib/features/ships/ui/ship_detail.dart
+++ lib/features/ships/ui/ship_detail.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:spacex/core/constant/colors.dart';
-import 'package:spacex/core/themes/text_styles.dart';
+import 'package:spacex/core/widgets/text_style.dart';
+import 'package:spacex/features/company_info/ui/widgets/text_animation_company.dart';
 import 'package:spacex/features/ships/ui/widgets/detail_row.dart';
+import 'package:spacex/features/ships/ui/widgets/row_is_active.dart';
 import 'widgets/container_image.dart';
 import 'widgets/ship_detail_app_bar.dart';
 
@@ -12,6 +14,8 @@ class ShipDetails extends StatelessWidget {
   final int mass;
   final String type;
   final bool isNetworkConnected;
+  final bool isActive;
+  final String homePort;
   const ShipDetails({
     required this.shipImage,
     required this.shipName,
@@ -20,6 +24,8 @@ class ShipDetails extends StatelessWidget {
     required this.mass,
     required this.type,
     this.isNetworkConnected = true,
+    required this.isActive,
+    required this.homePort,
   });
 
   @override
@@ -28,25 +34,59 @@ class ShipDetails extends StatelessWidget {
       backgroundColor: AppColors.backgroundDarkBlue,
       appBar: shipDetailAppBar(shipName),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 12),
-            detailImageContainer(context, shipImage, shipName,isNetworkConnected ),
-            const Padding(
-              padding: EdgeInsets.only(left: 10.0, top: 15, bottom: 20),
-              child: Text(
-                'Details',
-                style: MyTextStyles.font30WhiteBold,
+        child: Padding(
+          padding:
+              const EdgeInsets.only(left: 20.0, top: 10, bottom: 20, right: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              detailImageContainer(
+                  context, shipImage, shipName, isNetworkConnected),
+              const SizedBox(
+                height: 20,
               ),
-            ),
-            detailsValuesRow('Year built :', yearBuilt),
-            detailsValuesRow('Mass Kg:', mass),
-            detailsValuesRow('Type :', type),
-            const SizedBox(
-              height: 30,
-            ),
-          ],
+              const TextColorAnimation(
+                textAnimated: 'Details :',
+                fontSize: 23,
+                padding: 0,
+                alignment: Alignment.centerLeft,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              textStyle('Is $shipName Active?', 17),
+              RowIsActive(isActive: isActive),
+              const SizedBox(
+                height: 10,
+              ),
+              textStyle('Home : $homePort', 17),
+              const SizedBox(
+                height: 15,
+              ),
+              const Divider(
+                endIndent: 30,
+                indent: 30,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const TextColorAnimation(
+                textAnimated: 'More Info :',
+                fontSize: 23,
+                padding: 0,
+                alignment: Alignment.centerLeft,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              detailsValuesRow('Year built :', yearBuilt),
+              detailsValuesRow('Mass Kg:', mass),
+              detailsValuesRow('Type :', type),
+              const SizedBox(
+                height: 15,
+              ),
+            ],
+          ),
         ),
       ),
     );
