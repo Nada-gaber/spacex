bdf1b35d7f3817d41d3ed0628926ec10e68a87e4
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2024-05-10 04:38:28 +0200

    loading widget

--- lib/core/widgets/custom_loading_widget.dart
+++ lib/core/widgets/custom_loading_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spacex/core/constant/images.dart';
 
 class CustomLoadingWidget extends StatelessWidget {
   final Color color;
@@ -8,8 +9,7 @@ class CustomLoadingWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: CircularProgressIndicator(
-      color: color,
-    ));
+        child: SizedBox(
+            height: 100, width: 100, child: Image.asset(MyImages.loading)));
   }
 }
