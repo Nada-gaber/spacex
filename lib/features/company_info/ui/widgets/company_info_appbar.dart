862ed43eebd7b24419107101230a3a785aa71bc4
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2024-05-10 10:51:28 +0200

    edit app bar company

--- lib/features/company_info/ui/widgets/company_info_appbar.dart
+++ lib/features/company_info/ui/widgets/company_info_appbar.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:spacex/core/widgets/custom_icon_button.dart';
 import '../../../../core/widgets/text_style.dart';
 
-companyInfoAppBar(BuildContext context,String summary) {
+companyInfoAppBar(BuildContext context,) {
   return AppBar(
     backgroundColor: Colors.transparent,
     title: Row(
@@ -12,32 +11,6 @@ companyInfoAppBar(BuildContext context,String summary) {
           'SpaceX',
           18,
         ),
-        CustomIconButton(
-          onTap: () {
-            showDialog(
-              context: context,
-              builder: (BuildContext context) => _buildPopupDialog(context,summary),
-            );
-          },
-          color: Colors.white,
-          icon: Icons.info_outline_rounded,
-          size: 24,
-          isFontAwesomeIcons: false,
-          backgroundColor: Colors.transparent,
-        ),
-      ],
-    ),
-  );
-}
-
-_buildPopupDialog(BuildContext context, String summary) {
-  return AlertDialog(
-    title: const Text('SpaceX'),
-    content: Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text(summary),
       ],
     ),
   );
