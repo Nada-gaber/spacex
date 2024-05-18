9adfc270840bedd1106bd925801fc97fc2b2e7a5
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2024-05-19 00:45:22 +0200

    add colors nav,status to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:spacex/core/constant/colors.dart';
 import 'package:spacex/core/routing/app_router.dart';
 import 'package:spacex/core/utils/dependency_injection.dart';
 import 'package:spacex/core/utils/shared_preferences.dart';
@@ -10,6 +12,13 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setupGetIt();
   await MySharedPreferences.init();
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    systemNavigationBarColor: AppColors.backgroundDarkBlue,
+    systemNavigationBarIconBrightness: Brightness.light,
+    statusBarBrightness: Brightness.light,
+    statusBarIconBrightness: Brightness.light,
+    statusBarColor: AppColors.backgroundDarkBlue,
+  ));
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
